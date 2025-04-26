cf5ed79cdbb4a51f27a98a197a332e849e83fd68
AmnaRais <k230824@nu.edu.pk>
2025-04-26 15:43:38 +0500

    Update communication.h
    Added global constants like PAGE_SIZE, NUM_FRAMES, and shared data structures.

--- code (with sockets)/communication.h
+++ code (with sockets)/communication.h
@@ -1,6 +1,6 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,6 +9,10 @@
 #include <sys/socket.h>        // Socket functions
 #include <netinet/in.h>        // sockaddr_in structure
 #include <arpa/inet.h>
+
+#define NUM_FRAMES 64        
+#define PAGE_SIZE 4096        // 4KB per page
+#define PAGES_PER_JOB 4  
 
 // JOB INFO
 #define MAX_JOBS 10
@@ -22,6 +26,26 @@
 #define PRIORITY 3
 
 
+// Page table entry
+typedef struct
+{
+    int frame;          // Physical frame number (-1 if not in memory)
+    int is_used;        // 1 if page is allocated, 0 if free
+    int is_modified;    // For tracking if data was changed
+} PageTableEntry;
+
+
+typedef struct
+{
+PageTableEntry pages[PAGES_PER_JOB];  // Each job has fixed pages
+    int job_id;                           // Links to Job struct
+    int client_socket;                    // Which client owns this  
+} JobMemory;
+
+extern JobMemory *job_memories[MAX_JOBS];  // Maps job_id → memory
+extern char phys_mem[NUM_FRAMES][PAGE_SIZE]; // Physical memory
+extern int used_frames;  
+
 // Message structure
 struct message
 {
@@ -32,6 +56,7 @@ struct message
     int job_type;
     int priority;  
 };
+
 
 // Job structure
 typedef struct
@@ -44,7 +69,7 @@ typedef struct
     int job_type;
     int priority;                 // Priority for scheduling
     time_t arrival_time;    
-    time_t completion_time; 
+    time_t completion_time;
     int execution_time;
 } Job;
 
@@ -71,5 +96,7 @@ void* handle_client(void* arg);
 void set_scheduling_algorithm(int algorithm);
 void sort_queue_by_priority(void);
 void write_queue_to_log();
+void load_job_pages_into_memory(Job job, int thread_id);
+
 
 #endif
